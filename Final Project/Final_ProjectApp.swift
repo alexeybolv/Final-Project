c6f877d4c775a99e4e2455e5f2fd71f2ba49a347
Alexey Bolvonovich <alexeybolv@gmail.com>
2021-07-04 21:43:05 +0300

    feat: init project

---
+++ Final Project/Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_ProjectApp.swift
+//  Final Project
+//
+//  Created by Alexey Bolvonovich on 4.07.21.
+//
+
+import SwiftUI
+
+@main
+struct Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
