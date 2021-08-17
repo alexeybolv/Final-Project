936b81fc0ed312eee5e1d22d931e50c2b0e6632d
Alexey Bolvonovich <alexeybolv@gmail.com>
2021-08-17 19:54:02 +0300

    feat: add calculator screen

---
+++ Final Project/Calculators/Calculator/UI/CalculatorScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  CalculatorScreenView.swift
+//  Final Project
+//
+//  Created by Alexey Bolvonovich on 17.08.21.
+//
+
+import SwiftUI
+
+struct CalculatorScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CalculatorScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalculatorScreenView()
+    }
+}
